b5b7984003fadd0d46637ee7ea312ab541c8fa33
Stiven Trizky Katuuk <pemudakoding@gmail.com>
2022-11-23 20:49:00 +0800

    refactor: pint

--- src/Domain/Shared/User/Models/User.php
+++ src/Domain/Shared/User/Models/User.php
@@ -9,15 +9,18 @@ use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
 {
-    use HasApiTokens, Notifiable ;
-    use Authenticatable, Authorizable, CanResetPassword, MustVerifyEmail;
+    use HasApiTokens;
+    use Notifiable;
+    use Authenticatable;
+    use Authorizable;
+    use CanResetPassword;
+    use MustVerifyEmail;
 
     /**
      * The attributes that should be hidden for serialization.
