076273ae907c16844aed28ae953a24cee40dcec3
Kevin Abrar Khansa <kevariable@gmail.com>
2022-11-25 23:32:55 +0700

    build(testing): default pest, dto, user factory default and set pint

---
+++ tests/Feature/Internal/Concerns/DataTransferObject/ExampleData.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests\Feature\Internal\Concerns\DataTransferObject;
+
+use Domain\Shared\Foundation\DataTransferObject;
+
+class ExampleData extends DataTransferObject
+{
+    //
+}
